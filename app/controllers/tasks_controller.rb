e31c923ba9922d3c9b4493db615bd2345c7ac058
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-06-13 09:23:25 +0900

    登録アクション（create）を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,16 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
 end
