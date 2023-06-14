d3fc0dc36c63cd9277d877e4e70aba403e0e0341
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-06-14 10:52:25 +0900

    nameカラムにNOT NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_055456) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
